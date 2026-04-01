f17f126bd3e270a32b2604bdfc19f0239b10d5f6
Abhinav Naman <abhinavnaman3@gmail.com>
2026-04-01 23:41:29 +0530

    update inventory api

--- src/api/inventory.routes.ts
+++ src/api/inventory.routes.ts
@@ -17,6 +17,10 @@ router.get("/:id", async (req, res) => {
   const secret = await prisma.secret.findUnique({
     where: { id: req.params.id }
   })
+
+  if (!secret) {
+    return res.status(404).json({ message: "Secret not found" })
+  }
   res.json(secret)
 })
 
