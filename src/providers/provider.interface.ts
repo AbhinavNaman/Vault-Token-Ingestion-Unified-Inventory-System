5b48b0313c5af5a23e5858b2e68bb4d92494a5f1
Abhinav Naman <abhinavnaman3@gmail.com>
2026-04-01 23:40:46 +0530

    add TokenData interface

--- src/providers/provider.interface.ts
+++ src/providers/provider.interface.ts
@@ -1,12 +1,20 @@
+export interface TokenData {
+  accessor: string
+  policies: string[]
+  ttl?: number
+  createdTime?: Date
+}
+
 export interface SecretData {
   path: string
   version?: number
   ttl?: number
-  createdTime?: Date | undefined
+  createdTime?: Date
   owner?: string
 }
 
 export interface VaultProvider {
   getProviderName(): string
   fetchSecrets(): Promise<SecretData[]>
+  fetchTokens?(): Promise<TokenData[]>
 }
